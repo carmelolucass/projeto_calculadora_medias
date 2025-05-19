6f6c13939f6d0270d53d96883a0cfec2e6284aba
lucas melo <lucasmelo080698@gmail.com>
2025-05-19 12:39:52 -0300

    fix limitação de casas decimais

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function atualizaTabela(){
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2); /*2 casas decimais*/
     document.getElementById('media-final-resultado').innerHTML = 
         mediaFinal >= notaMinima ? spanAprovado : spanReprovado; // CORREÇÃO AQUI
 }
